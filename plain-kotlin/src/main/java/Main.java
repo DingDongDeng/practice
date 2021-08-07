f22e592d8a3f990b77cd341ba7980d9ccdfbb5ee
DingDongDeng <etea583@naver.com>
2021-08-07 16:09:13 +0900

    [plain-kotlin] 주석 추가

--- plain-kotlin/src/main/java/Main.java
+++ plain-kotlin/src/main/java/Main.java
@@ -1,7 +1,19 @@
+import calculator.Calculator;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!!!");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("start java calculator");
+        String formula = scanner.nextLine();
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.calc(formula));
+
+
+
     }
+
 
 }
--- plain-kotlin/src/main/kotlin/calculator/Main.kt
+++ plain-kotlin/src/main/kotlin/calculator/Main.kt
@@ -4,7 +4,16 @@ import java.util.*
 
 fun main(args: Array<String>) {
 
-    println("Hello My Calculator")
+    /**
+     *TODO
+     * null 관련된 핸들링을 해보자...
+     * :Int? 인데 일부로 null도 넣어보고... 어디까지 컴파일에서 잡아주는지도...
+     */
+    val MODE = "KOTLIN"
+    println(
+        """Hello,
+            |This is ${MODE.toUpperCase()} Calculator""".trimMargin()
+    )
     println("please, input formual for calculate")
     val calculator = Calculator()
     val scaner = Scanner(System.`in`)
